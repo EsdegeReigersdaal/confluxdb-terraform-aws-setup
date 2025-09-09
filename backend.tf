f0698500a3c0ffe06e6abd2f1d6523ec1de9b5ef
Michael Cheung <michael.cheung@esdege-reigersdaal.nl>
2025-09-09 15:31:01 +0200

    chore: updated lockfiles in .hcl files

--- backend.tf
+++ backend.tf
@@ -6,5 +6,6 @@ terraform {
     bucket       = "confluxdb-prod-tfstate-228407934486-eu-west-1"
     key          = "confluxdb/terraform.tfstate"
     region       = "eu-west-1"
+    use_lockfile = "true"
   }
 }
--- environments/prod.backend.hcl
+++ environments/prod.backend.hcl
@@ -1,3 +1,4 @@
 bucket = "confluxdb-prod-tfstate-228407934486-eu-west-1"
 key    = "confluxdb/terraform.tfstate"
 region = "eu-west-1"
+use_lockfile = "true"
